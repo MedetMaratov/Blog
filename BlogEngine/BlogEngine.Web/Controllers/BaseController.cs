62e15934ad25dec4deb229df7e65a4a0c2571429
Medet Maratov <maratovmedet75@gmail.com>
2023-07-08 20:09:27 +0600

    Log setting changed

--- BlogEngine/BlogEngine.Web/Controllers/BaseController.cs
+++ BlogEngine/BlogEngine.Web/Controllers/BaseController.cs
@@ -1,19 +1,19 @@
-﻿using MediatR;
-using Microsoft.AspNetCore.Mvc;
-using System.Security.Claims;
+﻿    using MediatR;
+    using Microsoft.AspNetCore.Mvc;
+    using System.Security.Claims;
 
-namespace BlogEngine.Web.Controllers
-{
-    [ApiController]
-    [Route("[controller]/[action]")]
-    public abstract class BaseController : Controller
+    namespace BlogEngine.Web.Controllers
     {
-        private IMediator _mediator;
-        protected IMediator Mediator => _mediator ??= HttpContext.RequestServices.GetService<IMediator>();
+        [ApiController]
+        [Route("[controller]/[action]")]
+        public abstract class BaseController : Controller
+        {
+            private IMediator _mediator;
+            protected IMediator Mediator => _mediator ??= HttpContext.RequestServices.GetService<IMediator>();
 
-        internal Guid UserId => !User.Identity.IsAuthenticated
-            ? Guid.Empty
-            : Guid.Parse(User.FindFirst(ClaimTypes.NameIdentifier).Value);
+            internal Guid UserId => !User.Identity.IsAuthenticated
+                ? Guid.Empty
+                : Guid.Parse(User.FindFirst(ClaimTypes.NameIdentifier).Value);
+        }
+
     }
-
-}
--- BlogEngine/BlogEngine.Web/Program.cs
+++ BlogEngine/BlogEngine.Web/Program.cs
@@ -1,9 +1,11 @@
 using BlogEngine.Domain.Entities;
 using BlogEngine.Persistence;
+using BlogEngine.Web.Models;
 using BlogEngine.Web.Services;
 using BlogEngineApplication;
 using BlogEngineApplication.Common.Mapping;
 using BlogEngineApplication.Interfaces;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Serilog;
@@ -36,7 +38,7 @@ services.AddHttpContextAccessor();
 
 Log.Logger = new LoggerConfiguration()
     .MinimumLevel.Override("Microsoft", LogEventLevel.Information)
-    .WriteTo.File("BlogWebAppLog-.txt", rollingInterval: RollingInterval.Day)
+    .WriteTo.File("Logs/BlogWebAppLog-.txt", rollingInterval: RollingInterval.Day)
     .CreateLogger();
 builder.Host.UseSerilog();
 var app = builder.Build();
@@ -53,7 +55,8 @@ using (var scope = app.Services.CreateScope())
         Log.Fatal(exception, "An error occured while app initialization");
     }
 }
-
+app.UseAuthentication();
+app.UseAuthorization();
 app.UseRouting();
 app.UseHttpsRedirection();
 
